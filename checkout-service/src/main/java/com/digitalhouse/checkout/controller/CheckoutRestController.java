6d5842a0f376df60f794c467aab3327db3559fec
GonzaloGollo <gonzalogollo@gmail.com>
2023-11-07 08:25:04 -0300

    Configuracion Gateway 2

--- checkout-service/src/main/java/com/digitalhouse/checkout/controller/CheckoutRestController.java
+++ checkout-service/src/main/java/com/digitalhouse/checkout/controller/CheckoutRestController.java
@@ -2,10 +2,8 @@ package com.digitalhouse.checkout.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.servlet.ServletOutputStream;
+import org.springframework.web.bind.annotation.*;
 
 import com.digitalhouse.checkout.model.Checkout;
 import com.digitalhouse.checkout.service.ICheckoutService;
@@ -25,7 +23,11 @@ public class CheckoutRestController {
 
 
 	@GetMapping()
-	public Checkout getCheckout(@RequestParam List<String> productIds) {
+	public Checkout getCheckout(@RequestParam List<String> productIds, @RequestHeader("X-Request-from") String requestFrom) {
+		System.out.println("Enviado desde: " + requestFrom);
+		if (!requestFrom.equals("gateway")){
+			return null;
+		}
 		
 		return checkoutService.buildCheckout(productIds);
 		
